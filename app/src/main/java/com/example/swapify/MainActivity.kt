b918d6622229d345c2dff3553912700d6fed9835
dantheman8300 <daniel.7039@hotmail.com>
2023-05-20 18:24:02 -0700

    start of spotify auth. Running into issue with redirect url

--- app/src/main/java/com/example/swapify/MainActivity.kt
+++ app/src/main/java/com/example/swapify/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.swapify
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -11,36 +13,60 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.swapify.ui.theme.SwapifyTheme
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
 
 class MainActivity : ComponentActivity() {
+    val REQUEST_CODE = 1337
+    val REDIRECT_URI = "swapify"
+    val CLIENT_ID = "338e7ddbbb344761b87776da268adf5f"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SwapifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    ConnectButton { initiateSpotifyAuthorization() }
                 }
+            }
+        }
+    }
+
+    private fun initiateSpotifyAuthorization() {
+        val builder = AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI)
+        builder.setScopes(arrayOf("streaming"))
+        val request = builder.build()
+
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            val response = AuthorizationClient.getResponse(resultCode, intent)
+            when (response.type) {
+                AuthorizationResponse.Type.TOKEN -> {}
+                AuthorizationResponse.Type.ERROR -> {}
+                else -> {}
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ConnectButton(onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text = "Connect to Spotify")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewConnectButton() {
     SwapifyTheme {
-        Greeting("Android")
+        ConnectButton {}
     }
 }
