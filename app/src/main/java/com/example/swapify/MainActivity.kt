2d4acbcd0f162a9bb91d61bea2309e084a6f52d1
dantheman8300 <daniel.7039@hotmail.com>
2023-06-02 14:19:59 -0700

    start of spotify auth. Running into issue with redirect url

--- app/src/main/java/com/example/swapify/MainActivity.kt
+++ app/src/main/java/com/example/swapify/MainActivity.kt
@@ -13,13 +13,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.swapify.ui.theme.SwapifyTheme
-import com.spotify.sdk.android.auth.AuthorizationClient
-import com.spotify.sdk.android.auth.AuthorizationRequest
-import com.spotify.sdk.android.auth.AuthorizationResponse
 
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
 class MainActivity : ComponentActivity() {
     val REQUEST_CODE = 1337
-    val REDIRECT_URI = "swapify"
+    val REDIRECT_URI = "http://localhost:8080"
     val CLIENT_ID = "338e7ddbbb344761b87776da268adf5f"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,32 +31,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             SwapifyTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    ConnectButton { initiateSpotifyAuthorization() }
                 }
             }
         }
     }
 
-    private fun initiateSpotifyAuthorization() {
-        val builder = AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI)
-        builder.setScopes(arrayOf("streaming"))
-        val request = builder.build()
-
-        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request)
+    override fun onStart() {
+        super.onStart()
+        // We will start writing our code here.
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
-        super.onActivityResult(requestCode, resultCode, intent)
+    private fun connected() {
+        // Then we will write some more code here.
+    }
 
-        // Check if result comes from the correct activity
-        if (requestCode == REQUEST_CODE) {
-            val response = AuthorizationClient.getResponse(resultCode, intent)
-            when (response.type) {
-                AuthorizationResponse.Type.TOKEN -> {}
-                AuthorizationResponse.Type.ERROR -> {}
-                else -> {}
-            }
-        }
+    override fun onStop() {
+        super.onStop()
+        // Aaand we will finish off here.
     }
 }
 
