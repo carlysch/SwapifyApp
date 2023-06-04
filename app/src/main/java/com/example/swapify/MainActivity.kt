92a035e748aa5805bd827a5b44d0ae75aba92a85
dantheman8300 <daniel.7039@hotmail.com>
2023-06-04 15:45:17 -0700

    attempt at new auth

--- app/src/main/java/com/example/swapify/MainActivity.kt
+++ app/src/main/java/com/example/swapify/MainActivity.kt
@@ -21,20 +21,72 @@ import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
+
 class MainActivity : ComponentActivity() {
     val REQUEST_CODE = 1337
     val REDIRECT_URI = "http://localhost:8080"
     val CLIENT_ID = "338e7ddbbb344761b87776da268adf5f"
 
+    val connectionParams = ConnectionParams.Builder(CLIENT_ID)
+        .setRedirectUri(REDIRECT_URI)
+        .showAuthView(true)
+        .setRequiredFeatures(arrayListOf("app-remote-control"))
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SwapifyTheme {
+                var spotifyAppRemote: SpotifyAppRemote
+
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    ConnectButton {
+//                        initiateSpotifyAuthorization()
+
+                        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+                            override fun onConnected(appRemote: SpotifyAppRemote) {
+                                spotifyAppRemote = appRemote
+                                println("MainActivity" + "Connected! Yay!")
+                                // Now you can start interacting with App Remote
+                                connected()
+                            }
+
+                            override fun onFailure(throwable: Throwable) {
+                                println("MainActivity error ${throwable.message} $throwable")
+                                // Something went wrong when attempting to connect! Handle errors here
+                            }
+                        })
+                    }
                 }
             }
         }
     }
+
+    private fun initiateSpotifyAuthorization() {
+        val builder = AuthorizationRequest.Builder(CLIENT_ID, AuthorizationResponse.Type.TOKEN, REDIRECT_URI)
+        builder.setScopes(arrayOf("streaming"))
+        val request = builder.build()
+
+        AuthorizationClient.openLoginActivity(this, REQUEST_CODE, request)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            val response = AuthorizationClient.getResponse(resultCode, intent)
+            when (response.type) {
+                AuthorizationResponse.Type.TOKEN -> {}
+                AuthorizationResponse.Type.ERROR -> {}
+                else -> {}
+            }
+        }
+    }
+
 
     override fun onStart() {
         super.onStart()
